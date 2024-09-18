e5d1e97d01c3be2a899411f4223d25913133179a
Karsten Walraet <karsten.walraet@gmail.com>
2024-09-18 14:50:29 +0200

    rajout de l'icone, du texte et de la barre noir pour la page administrateur dans login.js

--- js/login.js
+++ js/login.js
@@ -6,11 +6,30 @@ const inputMail = document.getElementById("email");
 const inputPassword = document.getElementById("password");
 const loginForm = document.getElementById("loginform");
 
+// Fonction pour afficher la barre noire de mode édition
+function afficherBlackBar() {
+  const blackBar = document.createElement("div");
+  blackBar.classList.add("black-bar");
+
+  // Créer l'icône
+  const icon = document.createElement("i");
+  icon.classList.add("fa-solid", "fa-pen-to-square");
+  icon.style.marginRight = "10px"; // Ajouter un espacement entre l'icône et le texte
+
+  // Ajouter l'icône et le texte à la barre
+  blackBar.appendChild(icon);
+  blackBar.appendChild(document.createTextNode("Mode édition"));
+
+  // Ajouter la barre noire en haut du body
+  document.body.prepend(blackBar);
+}
+
 // Fonction pour mettre à jour le bouton "Login" ou "Logout" en fonction de l'état de connexion
 function updateLoginLogoutButton() {
   const authToken = localStorage.getItem("authToken");
   const loginLink = document.querySelector('nav a[href="login.html"]');
 
+  // Appliquer la classe appropriée pour le style des boutons
   if (loginLink) {
     loginLink.classList.add("nav-button");
   }
@@ -18,12 +37,16 @@ function updateLoginLogoutButton() {
   if (authToken && window.location.pathname.includes("index.html")) {
     // Si l'utilisateur est connecté, afficher "Logout"
     loginLink.innerText = "Logout";
+
+    // Afficher la black bar de mode édition
+    afficherBlackBar();
+
     loginLink.addEventListener("click", function (e) {
       e.preventDefault();
       // Supprimer le token du localStorage pour déconnecter l'utilisateur
       localStorage.removeItem("authToken");
       // Rediriger vers la page de login après déconnexion
-      window.location.href = "login.html";
+      window.location.href = "index.html";
     });
   } else {
     // Si l'utilisateur n'est pas connecté, afficher "Login"
